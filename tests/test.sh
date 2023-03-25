fa28459997d69bf0862c7901c5cae9cfeb770ad2
Badr Hammada <105392702+newdevelo@users.noreply.github.com>
2023-03-25 14:14:13 +0000

    upload

--- tests/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if gcc -Wall -Wextra -Werror -pedantic -std=gnu89 -Wno-format *.c tests/main.c -o printf; then
-    echo "Your output: "
-    ./printf
-else
-    echo "Compilation failed"
-fi
-
